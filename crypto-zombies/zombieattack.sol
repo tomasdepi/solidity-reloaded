aa6775056289c266cdf289c6c452c88063820866
Tomas De Pietro <tomas.depietro@naranjax.com>
2021-12-02 14:37:54 -0800

    attack function

--- crypto-zombies/zombieattack.sol
+++ crypto-zombies/zombieattack.sol
@@ -12,6 +12,21 @@ contract ZombieAttack is ZombieHelper {
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   }
 
-  function attack(uint _zombieId, uint _targetId) external {
+  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+
+    uint rand = randMod(100);
+
+    if (rand <= attackVictoryProbability) {
+      myZombie.winCount++;
+      myZombie.level++;
+      enemyZombie.lossCount++;
+      feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+    }else{
+      myZombie.lossCount++;
+      enemyZombie.winCount++;
+      _triggerCooldown(_zombie);
+    }
   }
 }
