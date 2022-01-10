fdfad99ddcaadc9cadcda70a046ed16efb48f9b2
Tomas De Pietro <tomas94depi@gmail.com>
2022-01-10 14:16:14 -0300

    inbox project

---
+++ inbox/compile.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts(':Inbox'); // second argument is number of different contracts we want to compile)
---
+++ inbox/test/inbox.test.js
@@ -0,0 +1,5 @@
+const assert = require('assert');
+const ganache = require('ganache-cli'); // for local ethereum test network
+const Web3 = require('web3');
+
+
