3cc138c98410b4d6ec96f4af03b59894ae0c9a13
Tomas De Pietro <tomas94depi@gmail.com>
2022-01-10 21:45:31 -0300

    remove unecesary method

--- inbox/contracts/Inbox.sol
+++ inbox/contracts/Inbox.sol
@@ -1,17 +1,14 @@
 pragma solidity 0.4.17;
 
 contract Inbox {
-    string public message; // storage variable
+  string public message; // storage variable
 
-    function Inbox(string initialMessage) public {
-        message = initialMessage;
-    }
+  function Inbox(string initialMessage) public {
+    message = initialMessage;
+  }
 
-    function setMessage(string newMessage) public {
-        message = newMessage; 
-    }
+  function setMessage(string newMessage) public {
+    message = newMessage; 
+  }
 
-    function getMessage() public view returns(string) {
-        return message;
-    }
 }
