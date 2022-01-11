8c9776ab928ad4ea7fca3e4760fd156c04c766ff
Tomas De Pietro <tomas94depi@gmail.com>
2022-01-10 21:45:13 -0300

    fix syntax error

--- inbox/compile.js
+++ inbox/compile.js
@@ -5,4 +5,4 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-module.exports = solc.compile(source, 1).contracts(':Inbox'); // second argument is number of different contracts we want to compile)
+module.exports = solc.compile(source, 1).contracts[':Inbox']; // second argument is number of different contracts we want to compile)
